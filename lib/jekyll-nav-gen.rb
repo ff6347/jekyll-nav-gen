cfb92e7c85f868b55b1fd13cf76155c9a9390fc3
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-10-08 22:00:25 +0200

    cleanup

--- lib/jekyll-nav-gen.rb
+++ lib/jekyll-nav-gen.rb
@@ -1,34 +1,17 @@
 Dir[File.dirname(__FILE__) + "/nav-gen/*.rb"].each {|file| require file[0..-4] }
 require "nokogiri"
 def inject(page)
-  # print "working on page"
   # puts page.inspect
   doc = Nokogiri.HTML(page.output)
-  # navul = doc.css('nav ul')
-  # puts navul
-  # home.add_next_sibling "<li>one</li>"
   navstr = ""
-
   doc.xpath("//h2").each do |node|
     txt = node.content
     id = node["id"]
     item = "<li class=\"menu-item\"><a href=\"##{id}\" class=\"jump-link\">#{txt}</a></li>\n"
-    # li = Nokogiri::HTML::Node.new('li',doc)
-    # li["class"] = "menu-item"
-    # a = Nokogiri::HTML::Node.new('a',doc)
-    # a["href"] = "##{id}"
-    # a.content = txt
-    # li.add_child(a)
-    # navul.children.add_child(li)
     navstr+=item
   end
-  # navul << navstr
-
-  # puts navstr
   navstr += "<li class=\"menu-item\" id=\"last-item\">"
-  # page.output = page.output.gsub(/<\/head>/i,"baz" )
   page.output = page.output.gsub(/(<li\ class\="menu\-item"\ id\="last\-item">)/i, navstr)
-  # puts page.output
 end
 
 Jekyll::Hooks.register :documents, :post_render do |page|
